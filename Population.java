a2cbbf6cd1026b5ac48b9cf571279486c63a64e0
Akira-Kazu <marvin2darren@gmail.com>
2025-11-09 15:38:24 +0700

    update populasi jadi arrlist

--- Population.java
+++ Population.java
@@ -1,22 +1,25 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Population {
-    private Kromosom[] populasi;
+    private ArrayList<Kromosom> populasi;
 
+        //ukuran populasi banyaknya kromosom
     public Population(int ukuranPopulasi,int FireStationNum, House[] lokasiLahanKosong,Random rdm){
-        populasi = new Kromosom[ukuranPopulasi];
+        populasi = new ArrayList<>();
+
          for (int i = 0; i < ukuranPopulasi; i++) {
-            populasi[i] = new Kromosom(lokasiLahanKosong, FireStationNum, rdm);//kenapa random diperlukan untuk memanggil kromosom
+        populasi.add(new Kromosom(lokasiLahanKosong, FireStationNum, rdm));//kenapa random diperlukan untuk memanggil kromosom
             //populasi[i].f();
         }
 
-        //mengembalikan kromosom dengan nilai fitness terendah
-         
+     
     }
-
+            //mengembalikan kromosom dengan nilai fitness terendah
      public Kromosom getTerbaik() {
-        Arrays.sort(populasi); // karena Kromosom sudah implement compareTo
-        return populasi[0];
+        Collections.sort(populasi);
+        return populasi.getFirst();
     }
 }
